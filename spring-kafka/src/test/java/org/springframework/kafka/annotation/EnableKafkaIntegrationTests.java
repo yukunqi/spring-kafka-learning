06daa07a29847ca4c183f1fe2fd6282a9042564d
Gary Russell <grussell@vmware.com>
2020-07-16 15:30:47 -0400

    Fix Race in Micrometer Metrics Test
    See https://github.com/spring-projects/spring-kafka/issues/1536
    
    Don't use a node-based metric in the test; it may be created after we
    capture the metrics.
    
    Remove Micrometer Test Diagnostics

--- spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
@@ -145,7 +145,6 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import io.micrometer.core.instrument.ImmutableTag;
-import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 
@@ -823,7 +822,7 @@ public class EnableKafkaIntegrationTests {
 					.count())
 						.isGreaterThan(0);
 
-			assertThat(this.meterRegistry.get("kafka.producer.node.incoming.byte.total")
+			assertThat(this.meterRegistry.get("kafka.producer.incoming.byte.total")
 					.tag("producerTag", "bytesString")
 					.tag("spring.id", "bytesStringProducerFactory.bsPF-1")
 					.functionCounter()
@@ -925,14 +924,7 @@ public class EnableKafkaIntegrationTests {
 		@SuppressWarnings("unchecked")
 		@Bean
 		public MeterRegistry meterRegistry() {
-			SimpleMeterRegistry reg = new SimpleMeterRegistry();
-			List<java.util.function.Consumer<Meter>> al =
-					KafkaTestUtils.getPropertyValue(reg, "meterAddedListeners", List.class);
-			List<java.util.function.Consumer<Meter>> rl =
-					KafkaTestUtils.getPropertyValue(reg, "meterRemovedListeners", List.class);
-			al.add(meter -> logger.warn("Added:   " + meter.getId()));
-			rl.add(meter -> logger.warn("Removed: " + meter.getId()));
-			return reg;
+			return new SimpleMeterRegistry();
 		}
 
 		@Bean
