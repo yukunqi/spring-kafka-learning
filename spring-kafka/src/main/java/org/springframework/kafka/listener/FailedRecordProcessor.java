90c87c06f0e4cc47be8be8448c2fa8d0432f10fc
Kwangyong Kim <banana.yong@gmail.com>
2020-08-17 02:12:45 +0900

    Add ConversionException in classifier to skip
    Resolves https://github.com/spring-projects/spring-kafka/issues/1566
    
    - ConversionException should be considered as fatal like MessageConversionException
    - Add ConversionException in FailedRecordProcessor.classifier to skip retrying
    - edit reference document

--- spring-kafka/src/main/java/org/springframework/kafka/listener/FailedRecordProcessor.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/FailedRecordProcessor.java
@@ -28,6 +28,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.classify.BinaryExceptionClassifier;
 import org.springframework.core.log.LogAccessor;
 import org.springframework.kafka.support.TopicPartitionOffset;
+import org.springframework.kafka.support.converter.ConversionException;
 import org.springframework.kafka.support.serializer.DeserializationException;
 import org.springframework.lang.Nullable;
 import org.springframework.messaging.converter.MessageConversionException;
@@ -128,6 +129,7 @@ public abstract class FailedRecordProcessor extends KafkaExceptionLogLevelAware 
 	 * <ul>
 	 * <li>{@link DeserializationException}</li>
 	 * <li>{@link MessageConversionException}</li>
+	 * <li>{@link ConversionException}</li>
 	 * <li>{@link MethodArgumentResolutionException}</li>
 	 * <li>{@link NoSuchMethodException}</li>
 	 * <li>{@link ClassCastException}</li>
@@ -150,6 +152,7 @@ public abstract class FailedRecordProcessor extends KafkaExceptionLogLevelAware 
 	 * <ul>
 	 * <li>{@link DeserializationException}</li>
 	 * <li>{@link MessageConversionException}</li>
+	 * <li>{@link ConversionException}</li>
 	 * <li>{@link MethodArgumentResolutionException}</li>
 	 * <li>{@link NoSuchMethodException}</li>
 	 * <li>{@link ClassCastException}</li>
@@ -195,6 +198,7 @@ public abstract class FailedRecordProcessor extends KafkaExceptionLogLevelAware 
 		Map<Class<? extends Throwable>, Boolean> classified = new HashMap<>();
 		classified.put(DeserializationException.class, false);
 		classified.put(MessageConversionException.class, false);
+		classified.put(ConversionException.class, false);
 		classified.put(MethodArgumentResolutionException.class, false);
 		classified.put(NoSuchMethodException.class, false);
 		classified.put(ClassCastException.class, false);
