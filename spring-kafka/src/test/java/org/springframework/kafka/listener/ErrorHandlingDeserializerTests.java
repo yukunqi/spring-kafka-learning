bc086dedd3c64b37dd785a5b6ccc8ba609cebe30
Gary Russell <grussell@pivotal.io>
2020-08-17 12:02:06 -0400

    GH-1565: ErrorHandlingDeserializer Extensions
    Resolves https://github.com/spring-projects/spring-kafka/issues/1565
    
    Previously extended `ErrorHandlingDeserializer` only worked if configured
    as a class rather than a class name.
    
    Spring Boot automatically converts the class names to classes but config
    outside of such an environment would not work.
    
    **cherry-pick to 2.5.x, 2.4.x, 2.3.x**

--- spring-kafka/src/test/java/org/springframework/kafka/listener/ErrorHandlingDeserializerTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/listener/ErrorHandlingDeserializerTests.java
@@ -171,7 +171,7 @@ public class ErrorHandlingDeserializerTests {
 		public ConsumerFactory<String, String> cf() {
 			Map<String, Object> props = KafkaTestUtils.consumerProps(TOPIC + ".g1", "false", embeddedKafka());
 			props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-			props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer2.class.getName());
+			props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ExtendedEHD.class.getName());
 			props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer2.class);
 			props.put(ErrorHandlingDeserializer2.KEY_DESERIALIZER_CLASS, FailSometimesDeserializer.class);
 			props.put(ErrorHandlingDeserializer2.VALUE_DESERIALIZER_CLASS, FailSometimesDeserializer.class.getName());
@@ -230,6 +230,10 @@ public class ErrorHandlingDeserializerTests {
 			}
 			return string;
 		}
+
+	}
+
+	public static class ExtendedEHD<T> extends ErrorHandlingDeserializer2<T> {
 
 	}
 
