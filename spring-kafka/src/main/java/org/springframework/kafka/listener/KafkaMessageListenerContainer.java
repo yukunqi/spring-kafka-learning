fbd9cfc4513bf82a57543f42f560f255f4566489
Gary Russell <grussell@vmware.com>
2020-08-27 12:08:55 -0400

    Fix Shutdown Race Causing Test Delays
    Don't poll the consumer if the container has already stopped.
    This causes excessive test run times.
    Local build time reduced from 7 minutes to less than 5.

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
@@ -1059,6 +1059,9 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 			}
 			pauseConsumerIfNecessary();
 			this.lastPoll = System.currentTimeMillis();
+			if (!isRunning()) {
+				return;
+			}
 			this.polling.set(true);
 			ConsumerRecords<K, V> records = doPoll();
 			if (!this.polling.compareAndSet(true, false) && records != null) {
