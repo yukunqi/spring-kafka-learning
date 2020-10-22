70af1ca175781896e90e50f2c6c0cd23ba47e9b7
Gary Russell <grussell@vmware.com>
2020-10-22 15:42:24 -0400

    Javadoc Polishing

--- spring-kafka/src/main/java/org/springframework/kafka/listener/RemainingRecordsErrorHandler.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/RemainingRecordsErrorHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 the original author or authors.
+ * Copyright 2017-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -40,7 +40,7 @@ public interface RemainingRecordsErrorHandler extends ConsumerAwareErrorHandler 
 	}
 
 	/**
-	 * Handle the exception.
+	 * Handle the exception. The failed record is the first in the list.
 	 * @param thrownException the exception.
 	 * @param records the remaining records including the one that failed.
 	 * @param consumer the consumer.
