27ddb42e71a9c2ddc13ac47195448f5d4ae69705
Artem Bilan <abilan@vmware.com>
2021-01-04 16:32:31 -0500

    Remove JavaDocs for non-existing generics
    **Cherry-pick to `2.6.x`**

--- spring-kafka-test/src/main/java/org/springframework/kafka/test/utils/KafkaTestUtils.java
+++ spring-kafka-test/src/main/java/org/springframework/kafka/test/utils/KafkaTestUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 the original author or authors.
+ * Copyright 2016-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -245,8 +245,6 @@ public final class KafkaTestUtils {
 
 	/**
 	 * Return the end offsets of the requested topic/partitions
-	 * @param <K> the key type.
-	 * @param <V> the value type.
 	 * @param consumer the consumer.
 	 * @param topic the topic.
 	 * @param partitions the partitions, or null for all partitions.
