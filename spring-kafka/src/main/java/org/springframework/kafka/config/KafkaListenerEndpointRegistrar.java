aa507b3e406bf32b947a33c2fbf85ceb8b1730e6
Gary Russell <grussell@vmware.com>
2021-01-12 13:41:51 -0500

    GH-1675: Validation on @KafkaHandler Methods
    Resolves https://github.com/spring-projects/spring-kafka/issues/1675
    
    Add support for payload validation with `@KafkaHandler` methods.
    
    * Fix javadoc.

--- spring-kafka/src/main/java/org/springframework/kafka/config/KafkaListenerEndpointRegistrar.java
+++ spring-kafka/src/main/java/org/springframework/kafka/config/KafkaListenerEndpointRegistrar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2020 the original author or authors.
+ * Copyright 2014-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -187,6 +187,10 @@ public class KafkaListenerEndpointRegistrar implements BeanFactoryAware, Initial
 	protected void registerAllEndpoints() {
 		synchronized (this.endpointDescriptors) {
 			for (KafkaListenerEndpointDescriptor descriptor : this.endpointDescriptors) {
+				if (descriptor.endpoint instanceof MultiMethodKafkaListenerEndpoint
+						&& this.validator != null) {
+					((MultiMethodKafkaListenerEndpoint) descriptor.endpoint).setValidator(this.validator);
+				}
 				this.endpointRegistry.registerListenerContainer(
 						descriptor.endpoint, resolveContainerFactory(descriptor));
 			}
