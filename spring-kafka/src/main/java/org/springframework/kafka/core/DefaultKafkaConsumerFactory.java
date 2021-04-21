d47d178cef1031df477b1fac06ce82fa7688286c
Gary Russell <grussell@vmware.com>
2021-04-21 10:50:26 -0400

    GH-1778: Properly Apply Client Post Processors
    Resolves https://github.com/spring-projects/spring-kafka/issues/1778
    
    `Consumer`/`Producer` post processors are `Function`s and the result should
    be used; it was previously discarded.
    
    **cherry-pick to 2.6.x, 2.5.x**

--- spring-kafka/src/main/java/org/springframework/kafka/core/DefaultKafkaConsumerFactory.java
+++ spring-kafka/src/main/java/org/springframework/kafka/core/DefaultKafkaConsumerFactory.java
@@ -324,7 +324,7 @@ public class DefaultKafkaConsumerFactory<K, V> extends KafkaResourceFactory
 			}
 		}
 		for (ConsumerPostProcessor<K, V> pp : this.postProcessors) {
-			pp.apply(kafkaConsumer);
+			kafkaConsumer = pp.apply(kafkaConsumer);
 		}
 		return kafkaConsumer;
 	}
