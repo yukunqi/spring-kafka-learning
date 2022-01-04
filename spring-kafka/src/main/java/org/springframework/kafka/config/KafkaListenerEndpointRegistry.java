3574c5c9cc15fe9dbe6f8dd149ecafa50e10c843
Gary Russell <grussell@vmware.com>
2022-01-04 13:38:38 -0500

    GH-2055: Containers Must Implement DisposableBean
    Resolves https://github.com/spring-projects/spring-kafka/issues/2055
    
    If context initialization fails, `Lifecycle.stop()` is not called.
    Containers must be stopped from `DisposableBean` in this case.
    
    **cherry-pick to 2.7.x, 2.6.x, 2.5.x**
    
    # Conflicts:
    #	spring-kafka/src/main/java/org/springframework/kafka/listener/MessageListenerContainer.java

--- spring-kafka/src/main/java/org/springframework/kafka/config/KafkaListenerEndpointRegistry.java
+++ spring-kafka/src/main/java/org/springframework/kafka/config/KafkaListenerEndpointRegistry.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2021 the original author or authors.
+ * Copyright 2014-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -242,14 +242,7 @@ public class KafkaListenerEndpointRegistry implements ListenerContainerRegistry,
 	@Override
 	public void destroy() {
 		for (MessageListenerContainer listenerContainer : getListenerContainers()) {
-			if (listenerContainer instanceof DisposableBean) {
-				try {
-					((DisposableBean) listenerContainer).destroy();
-				}
-				catch (Exception ex) {
-					this.logger.warn(ex, "Failed to destroy message listener container");
-				}
-			}
+			listenerContainer.destroy();
 		}
 	}
 
