9722b8420b670eb256dc9000e53e5269a66253d4
Gary Russell <grussell@vmware.com>
2022-01-27 15:49:04 -0500

    GH-2076: Fix Async Commit Retries
    https://github.com/spring-projects/spring-kafka/issues/2076
    
    Do not attempt to retry asynchronous commits.
    - a later commit for the same topic/partition may have already succeeded
    - many consecutive retryable exceptions can cause a stack overflow
    
    **cherry-pick to 2.8.x, 2.7.x**
    
    * Remove unused parameter; polish javadocs.
    # Conflicts:
    #	spring-kafka/src/test/java/org/springframework/kafka/listener/KafkaMessageListenerContainerTests.java

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
@@ -1452,7 +1452,7 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 								commitSync(toFix);
 							}
 							else {
-								commitAsync(toFix, 0);
+								commitAsync(toFix);
 							}
 						}
 						else {
@@ -1912,7 +1912,7 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 				commitSync(commits);
 			}
 			else {
-				commitAsync(commits, 0);
+				commitAsync(commits);
 			}
 		}
 
@@ -1931,18 +1931,14 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 				commitSync(commits);
 			}
 			else {
-				commitAsync(commits, 0);
+				commitAsync(commits);
 			}
 		}
 
-		private void commitAsync(Map<TopicPartition, OffsetAndMetadata> commits, int retries) {
+		private void commitAsync(Map<TopicPartition, OffsetAndMetadata> commits) {
 			this.consumer.commitAsync(commits, (offsetsAttempted, exception) -> {
-				if (exception instanceof RetriableCommitFailedException
-						&& retries < this.containerProperties.getCommitRetries()) {
-					commitAsync(commits, retries + 1);
-				}
-				else {
-					this.commitCallback.onComplete(offsetsAttempted, exception);
+				this.commitCallback.onComplete(offsetsAttempted, exception);
+				if (exception == null) {
 					if (this.fixTxOffsets) {
 						this.lastCommits.putAll(commits);
 					}
@@ -2701,7 +2697,7 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 						commitSync(offsetsToCommit);
 					}
 					else {
-						commitAsync(offsetsToCommit, 0);
+						commitAsync(offsetsToCommit);
 					}
 				}
 				else {
@@ -2963,7 +2959,7 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 						commitSync(commits);
 					}
 					else {
-						commitAsync(commits, 0);
+						commitAsync(commits);
 					}
 				}
 				catch (@SuppressWarnings(UNUSED) WakeupException e) {
@@ -3360,7 +3356,7 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 						}
 					}
 					else {
-						commitAsync(offsetsToCommit, 0);
+						commitAsync(offsetsToCommit);
 					}
 				}
 			}
