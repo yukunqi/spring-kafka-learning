6a253f23e7059c0a8a648c63502a91c1df59990c
Gary Russell <grussell@vmware.com>
2022-10-13 13:35:24 -0400

    GH-2432: Fix Retryable Topic Provisioning
    Resolves https://github.com/spring-projects/spring-kafka/issues/2432
    
    Don't provision an individual retry topic bean, if there is already a `NewTopic`
    bean with the same topic name.
    
    **cherry-pick to 2.9.x, 2.8.x**
    
    * Fix `TopicForRetry` removal logic; include `NewTopics` beans in logic.
    
    * Improve test.
    # Conflicts:
    #	spring-kafka/src/main/java/org/springframework/kafka/core/KafkaAdmin.java
    #	spring-kafka/src/main/java/org/springframework/kafka/retrytopic/RetryTopicConfigurer.java
    
    # Conflicts:
    #	spring-kafka/src/test/java/org/springframework/kafka/retrytopic/RetryTopicIntegrationTests.java

--- spring-kafka/src/main/java/org/springframework/kafka/retrytopic/RetryTopicConfigurer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/retrytopic/RetryTopicConfigurer.java
@@ -21,7 +21,6 @@ import java.util.Collection;
 import java.util.function.Consumer;
 
 import org.apache.commons.logging.LogFactory;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import org.springframework.beans.factory.BeanFactory;
@@ -35,6 +34,7 @@ import org.springframework.kafka.config.MethodKafkaListenerEndpoint;
 import org.springframework.kafka.config.MultiMethodKafkaListenerEndpoint;
 import org.springframework.kafka.listener.ListenerUtils;
 import org.springframework.kafka.support.EndpointHandlerMethod;
+import org.springframework.kafka.support.TopicForRetryable;
 import org.springframework.lang.Nullable;
 
 
@@ -355,7 +355,7 @@ public class RetryTopicConfigurer {
 				String beanName = topic + "-topicRegistrationBean";
 				if (!bf.containsBean(beanName)) {
 					bf.registerSingleton(beanName,
-							new NewTopic(topic, config.getNumPartitions(), config.getReplicationFactor()));
+							new TopicForRetryable(topic, config.getNumPartitions(), config.getReplicationFactor()));
 				}
 			}
 		);
