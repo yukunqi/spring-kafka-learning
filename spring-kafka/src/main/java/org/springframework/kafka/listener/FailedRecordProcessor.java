59eb2dccab277bf3a52bfb2fb56630d45e805379
Gary Russell <grussell@vmware.com>
2022-10-27 13:44:55 -0400

    GH-2464: Fix Delivery Attempt Header (Rare)
    Resolves https://github.com/spring-projects/spring-kafka/issues/2464
    
    `KafkaBackoffException` should never be classified as retryable; the design
    is such that those exceptions go straight to the recoverer, which will re-throw.
    
    If, however, a user configures a super class as retryable (e.g. `RuntimeException`)
    then its classification wins.
    
    This causes a new retry state (`FailedRecord`) to be started, causing an incorrect
    value of 2 instead of 1 on the first delivery from a retry topic.
    
    **I will backport - conflicts expected**

--- spring-kafka/src/main/java/org/springframework/kafka/listener/FailedRecordProcessor.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/FailedRecordProcessor.java
@@ -46,7 +46,7 @@ public abstract class FailedRecordProcessor extends ExceptionClassifier implemen
 	private final BiFunction<ConsumerRecord<?, ?>, Exception, BackOff> noRetriesForClassified =
 			(rec, ex) -> {
 				Exception theEx = ErrorHandlingUtils.unwrapIfNeeded(ex);
-				if (!getClassifier().classify(theEx)) {
+				if (!getClassifier().classify(theEx) || theEx instanceof KafkaBackoffException) {
 					return NO_RETRIES_OR_DELAY_BACKOFF;
 				}
 				return this.userBackOffFunction.apply(rec, ex);
