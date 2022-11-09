5df71c169a366bee623780a9b99a36f3055b485c
Gary Russell <grussell@vmware.com>
2022-11-09 14:43:03 -0500

    Fix Missing Re-Interrupt

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
@@ -1605,6 +1605,7 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 				ListenerUtils.stoppableSleep(KafkaMessageListenerContainer.this, duration.toMillis());
 			}
 			catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
 				this.logger.error(e, "Interrupted while sleeping");
 			}
 		}
