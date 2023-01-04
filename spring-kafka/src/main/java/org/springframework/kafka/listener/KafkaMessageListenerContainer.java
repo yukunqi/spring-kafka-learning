f3cfa198b088497b684ae3bf8bac5d5a500bba1e
Gary Russell <grussell@vmware.com>
2023-01-04 13:59:47 -0500

    GH-2525: Fix Paused Partition Resume (Rebalance)
    Resolves https://github.com/spring-projects/spring-kafka/issues/2525
    
    If a paused partition is revoked and re-assigned during a retry topic
    pause delay, the partition is not resumed because it no longer
    exists in the `pausedPartitions` field.
    
    When re-pausing a paused partition, it must be re-added to the paused
    partitions so that it will not be filtered out of the resume logic.
    
    **cherry-pick to 2.9.x**

--- spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
+++ spring-kafka/src/main/java/org/springframework/kafka/listener/KafkaMessageListenerContainer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2022 the original author or authors.
+ * Copyright 2016-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -3596,6 +3596,7 @@ public class KafkaMessageListenerContainer<K, V> // NOSONAR line count
 					ListenerConsumer.this.consumer.pause(toRepause);
 					ListenerConsumer.this.logger.debug(() -> "Paused consumption from: " + toRepause);
 					publishConsumerPausedEvent(toRepause, "Re-paused after rebalance");
+					ListenerConsumer.this.pausedPartitions.addAll(toRepause);
 				}
 				this.revoked.removeAll(toRepause);
 				ListenerConsumer.this.pausedPartitions.removeAll(this.revoked);
